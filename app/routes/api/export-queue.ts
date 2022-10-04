9fcf595cf7bde65fee2fc71cb8dc1146dc3b5ae6
yairdyule <jaje9434@colorado.edu>
2022-10-03 22:21:54 -0600

    format

--- app/routes/api/export-queue.ts
+++ app/routes/api/export-queue.ts
@@ -4,36 +4,39 @@ import { requireSpotifyToken, requireUser } from "~/session.server";
 import { Spotify } from "~/utils/spotify";
 
 export const action: ActionFunction = async ({ request, params }) => {
-  const token = await requireSpotifyToken(request);
+  try {
+    const token = await requireSpotifyToken(request);
 
-  const user = await Spotify.getUserData({ auth_token: token });
+    const user = await Spotify.getUserData({ auth_token: token });
 
-  const { id } = await requireUser(request);
+    const { id } = await requireUser(request);
 
-  const fd = await request.formData();
-  const queueId = fd.get("queueId")?.toString() || "";
-  const queue = await prisma.queue.findUnique({
-    where: { id: queueId },
-    include: { songs: true },
-  });
+    const fd = await request.formData();
+    const queueId = fd.get("queueId")?.toString() || "";
+    const queue = await prisma.queue.findUnique({
+      where: { id: queueId },
+      include: { songs: true },
+    });
 
-  const playlist = await Spotify.createPlaylist(
-    {
-      name: queue?.title || "Made with Sendit",
-      description: queue?.description,
-      userId: user.id,
-      isPublic: false,
-    },
-    token
-  );
+    const playlist = await Spotify.createPlaylist(
+      {
+        name: queue?.title || "Made with Sendit",
+        description: queue?.description,
+        userId: user.id,
+        isPublic: false,
+      },
+      token
+    );
 
-  await Spotify.populatePlaylist(
-    {
-      playlistId: playlist.id,
-      trackUris: queue?.songs.map((s) => s.spotify_uri) || [],
-    },
-    token
-  );
-
+    await Spotify.populatePlaylist(
+      {
+        playlistId: playlist.id,
+        trackUris: queue?.songs.map((s) => s.spotify_uri) || [],
+      },
+      token
+    );
+  } catch (error) {
+    throw error;
+  }
   return redirect("/received");
 };
