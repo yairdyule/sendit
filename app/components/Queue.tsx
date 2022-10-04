280fe939e50fc1001a9559a511c1b9a51037821d
yairdyule <jaje9434@colorado.edu>
2022-10-04 00:05:26 -0600

    revamp to handle feed v. sent page cases

--- app/components/Queue.tsx
+++ app/components/Queue.tsx
@@ -1,26 +1,33 @@
 import { SquaresPlusIcon } from "@heroicons/react/24/solid";
 import { Link } from "@remix-run/react";
 
-import type { Queue } from "@prisma/client";
+import type { Queue, User } from "@prisma/client";
 
-export function QueueCard({ queue }: { queue: Queue | any }) {
+export function QueueCard({
+  queue,
+  author,
+}: {
+  queue: Queue | any;
+  author?: User | any;
+}) {
   return (
-    <li
-      key={queue.title}
-      className="col-span-1 divide-y divide-gray-200 rounded-lg bg-zinc-800 shadow-sm shadow-zinc-900 transition-all"
-    >
-      <div className="flex w-full items-center justify-between space-x-6 p-6">
-        <div className="flex-1 truncate">
-          <div className="flex items-center space-x-3">
-            <h3 className="text-md truncate font-medium text-gray-200">
-              {queue.title}
+    <div className="flex w-full max-w-xl items-center justify-between space-x-6 rounded-md bg-zinc-800 p-6">
+      <div className="flex-1 truncate">
+        <div className="flex w-72 items-center space-x-3">
+          <h3 className="text-md truncate font-medium text-gray-200">
+            {queue.title}
+          </h3>
+          {author && (
+            <h3 className="text-md truncate font-medium text-gray-400">
+              {author.email}
             </h3>
-          </div>
-          <p className="truncate text-sm text-gray-400">{queue.description}</p>
-          <span className="inline-block flex-shrink-0 items-center justify-center rounded-full bg-emerald-800 px-2 py-0.5 text-xs  text-zinc-200"></span>
+          )}
         </div>
+        <span className="inline-block flex-shrink-0 items-center justify-center rounded-full bg-emerald-800 px-2 py-0.5 text-xs  text-zinc-200">
+          {new Date(queue.updatedAt).toLocaleDateString()}
+        </span>
       </div>
-    </li>
+    </div>
   );
 }
 
@@ -31,7 +38,7 @@ export function AddQueueCard() {
         <div className="flex-1 truncate">
           <Link
             className="flex flex-col items-start justify-center gap-1"
-            to="/sent/add"
+            to="/created/add"
           >
             <h3 className="text-md inline-flex items-center truncate font-medium text-gray-400 ">
               Create a queue{" "}
