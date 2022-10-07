c7ef8db7ec4eba9b23fb426a79b8f786e020bf45
yairdyule <jaje9434@colorado.edu>
2022-10-06 20:05:12 -0600

    style(feed): change overflow type

--- app/routes/feed.tsx
+++ app/routes/feed.tsx
@@ -25,7 +25,7 @@ export default function Homepage() {
   return (
     <Layout>
       <Wrapper>
-        <h2 className="overflow-scroll text-xl font-medium text-emerald-400">
+        <h2 className="overflow-auto text-xl font-medium text-emerald-400">
           Feed
         </h2>
         <div className="flex flex-col gap-4 overflow-auto pt-4">
