5a68f27ba45f5a8dc06e5d6013200e7f2245bfea
yairdyule <jaje9434@colorado.edu>
2022-10-07 21:43:39 -0600

    style: add desktop nav

--- app/components/Layout/index.tsx
+++ app/components/Layout/index.tsx
@@ -4,12 +4,45 @@ import { Form, NavLink } from "@remix-run/react";
 
 import { BiMailSend, BiUser } from "react-icons/bi";
 import { type IconType } from "react-icons";
+import { breakpoints, classNames } from "~/utils/css";
 
 export const Main = ({ children }: { children: React.ReactNode }) => {
+  console.log(
+    classNames(
+      "flex h-full w-full flex-col-reverse transition-colors",
+      breakpoints.mediumAndUp("flex-row")
+    )
+  );
+
   return (
-    <div className="flex h-full w-full flex-col-reverse transition-colors sm:flex-col">
-      <nav className="h-16 w-full border-t-[3px] border-emerald-500 sm:border-b-2 sm:border-t-transparent">
+    <div
+      className={classNames(
+        "flex h-full w-full flex-col-reverse transition-colors",
+        breakpoints.smallAndUp("flex-row")
+      )}
+    >
+      <nav
+        className={
+          "h-16 w-full border-t-[3px] border-emerald-500 sm:hidden sm:border-b-2 sm:border-t-transparent"
+        }
+      >
         <ul className="flex h-full flex-row items-center justify-center gap-8 md:gap-10">
+          <li>
+            <NavLinkWithIcon to="/created" Icon={BiMailSend} />
+          </li>
+          <li>
+            <NavLinkWithIcon to="/feed" Icon={NewspaperIcon} />
+          </li>
+          <li>
+            <NavLinkWithIcon to="/profile" Icon={BiUser} />
+          </li>
+        </ul>
+      </nav>
+      <nav
+        aria-label="Sidebar"
+        className="hidden md:block md:flex-shrink-0 md:overflow-y-auto md:bg-neutral-800 md:border-r md:border-r-emerald-600"
+      >
+        <ul className="relative flex w-20 flex-col items-center justify-center space-y-8 p-3">
           <li>
             <NavLinkWithIcon to="/created" Icon={BiMailSend} />
           </li>
