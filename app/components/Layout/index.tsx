628ce13a0fff541271eb83a60892502e46d14d2d
yairdyule <jaje9434@colorado.edu>
2022-10-09 09:32:03 -0600

    style: remove global container

--- app/components/Layout/index.tsx
+++ app/components/Layout/index.tsx
@@ -1,60 +1,72 @@
 import { NewspaperIcon } from "@heroicons/react/24/solid";
 import React from "react";
-import { Form, NavLink } from "@remix-run/react";
+import { NavLink } from "@remix-run/react";
 
 import { BiMailSend, BiUser } from "react-icons/bi";
+import { classNames } from "~/utils/css";
+
 import { type IconType } from "react-icons";
-import { breakpoints, classNames } from "~/utils/css";
+import type { ReactNode } from "react";
 
 export const Main = ({ children }: { children: React.ReactNode }) => {
   return (
     <div
       className={classNames(
-        "flex h-full w-full flex-col-reverse transition-colors sm:flex-row",
+        "flex h-full w-full flex-col-reverse transition-colors sm:flex-row"
       )}
     >
-      <nav
-        className={
-          "h-16 w-full border-t-[3px] border-emerald-500 sm:hidden sm:border-b-2 sm:border-t-transparent"
-        }
-      >
-        <ul className="flex h-full flex-row items-center justify-center gap-8 md:gap-10">
-          <li>
-            <NavLinkWithIcon to="/created" Icon={BiMailSend} />
-          </li>
-          <li>
-            <NavLinkWithIcon to="/feed" Icon={NewspaperIcon} />
-          </li>
-          <li>
-            <NavLinkWithIcon to="/profile" Icon={BiUser} />
-          </li>
-        </ul>
-      </nav>
-      <nav
-        aria-label="Sidebar"
-        className="hidden pt-6 sm:block sm:flex-shrink-0 sm:overflow-y-auto sm:border-r sm:border-r-card-lighter sm:bg-card-dark"
-      >
-        <ul className="relative flex w-20 flex-col items-center justify-center space-y-8 p-3">
-          <li>
-            <NavLinkWithIcon to="/created" Icon={BiMailSend} />
-          </li>
-          <li>
-            <NavLinkWithIcon to="/feed" Icon={NewspaperIcon} />
-          </li>
-          <li>
-            <NavLinkWithIcon to="/profile" Icon={BiUser} />
-          </li>
-        </ul>
-      </nav>
-      <div className="container mx-auto flex-grow overflow-scroll px-4 md:mx-0">
-        {children}
-      </div>
+      <SmallNav />
+      <SidebarNav />
+      <div className="h-full w-full">{children}</div>
     </div>
   );
 };
 
-export default {
-  Main,
+const SmallNav = () => (
+  <nav
+    className={
+      "h-16 w-full border-t-[3px] border-emerald-500 sm:hidden sm:border-b-2 sm:border-t-transparent"
+    }
+  >
+    <ul className="flex h-full flex-row items-center justify-center gap-8 md:gap-10">
+      <li>
+        <NavLinkWithIcon to="/created" Icon={BiMailSend} />
+      </li>
+      <li>
+        <NavLinkWithIcon to="/feed" Icon={NewspaperIcon} />
+      </li>
+      <li>
+        <NavLinkWithIcon to="/profile" Icon={BiUser} />
+      </li>
+    </ul>
+  </nav>
+);
+
+const SidebarNav = () => (
+  <nav
+    aria-label="Sidebar"
+    className="hidden pt-6 sm:block flex-shrink-0 overflow-y-auto border-r border-r-neutral-700 bg-card-dark"
+  >
+    <ul className="relative flex w-20 flex-col items-center justify-center space-y-8 p-3">
+      <li>
+        <NavLinkWithIcon to="/created" Icon={BiMailSend} />
+      </li>
+      <li>
+        <NavLinkWithIcon to="/feed" Icon={NewspaperIcon} />
+      </li>
+      <li>
+        <NavLinkWithIcon to="/profile" Icon={BiUser} />
+      </li>
+    </ul>
+  </nav>
+);
+
+const Container = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="container mx-auto flex-grow overflow-scroll px-4 md:mx-0">
+      {children}
+    </div>
+  );
 };
 
 const NavLinkWithIcon = ({ to, Icon }: { to: string; Icon: IconType }) => {
@@ -71,4 +83,9 @@ const NavLinkWithIcon = ({ to, Icon }: { to: string; Icon: IconType }) => {
       }}
     </NavLink>
   );
+};
+
+export default {
+  Main,
+  Container,
 };
