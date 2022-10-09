505fde59223bc620b65a8026bc73e0c4abdc1a91
yairdyule <jaje9434@colorado.edu>
2022-10-09 08:55:02 -0600

    style(profile): add profile header, svg

--- app/routes/profile/index.tsx
+++ app/routes/profile/index.tsx
@@ -1,20 +1,26 @@
-import { Form, useLoaderData } from "@remix-run/react";
+import { prisma } from "~/db.server";
+import dayjs from "dayjs";
+import LocalizedFormat from "dayjs/plugin/localizedFormat";
 import { json } from "@remix-run/server-runtime";
+import { useLoaderData } from "@remix-run/react";
 import { requireCreatedUser, requireSpotifyUser } from "~/session.server";
 
 import type { LoaderArgs } from "@remix-run/server-runtime";
-import { prisma } from "~/db.server";
-import { User } from "@prisma/client";
+import type { User } from "@prisma/client";
+
+dayjs.extend(LocalizedFormat);
 
 type LoaderData = {
   user: User;
-  sentCount: number;
+  sentQueueCount: number;
   gotCount: number;
 };
 export async function loader({ request }: LoaderArgs) {
   await requireSpotifyUser(request);
   const user = await requireCreatedUser(request);
-  const sentCount = await prisma.queue.count({ where: { authorId: user.id } });
+  const sentQueueCount = await prisma.queue.count({
+    where: { authorId: user.id },
+  });
   const gotCount = await prisma.queue.count({
     where: {
       recipientIds: {
@@ -23,37 +29,29 @@ export async function loader({ request }: LoaderArgs) {
     },
   });
 
-  return json<LoaderData>({ user, sentCount, gotCount });
+  return json<LoaderData>({ user, sentQueueCount, gotCount });
 }
 
 export default function ProfilePage() {
-  const { user, gotCount, sentCount } = useLoaderData<LoaderData>();
   return (
-    <div className="flex h-full flex-col gap-4">
-      <h2 className="text-lg font-bold text-emerald-400">{user.username}</h2>
-
-      <h2 className="text-lg font-bold text-emerald-400">
-        {sentCount > 0
-          ? `has sent ${sentCount} queue${sentCount > 1 ? "s" : ""} :D`
-          : "hasn't sent any queues"}
-      </h2>
-
-      <p className="text-emerald-400">
-        ok you're probably all like 'why isn't there more here' which is totally
-        fair but like c'mon man i made this in a weekend give me some slack. or
-        rather, ideas & feedback on how i can improve the experience, directed
-        to: jewellcommajared (at) gmail (dot) com. thanks for using SendIt, i
-        hope you have a wonderful day {"<3"}
-      </p>
-
-      <Form method="post" action="/logout">
-        <button
-          type="submit"
-          className="inline-flex items-center rounded-md border border-transparent bg-emerald-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        >
-          Logout
-        </button>
-      </Form>
+    <div className="flex h-full w-full flex-col gap-4">
+      <UserHeader />
     </div>
   );
 }
+
+const UserHeader = () => {
+  const { user, sentQueueCount, gotCount } = useLoaderData<LoaderData>();
+  return (
+    <header className="flex h-32 w-full flex-col items-start justify-end  bg-card-dark bg-[url('/topography.svg')] px-4 pb-2">
+      <h2 className="text-lg font-medium text-neutral-100">{user.username}</h2>
+      <div className="flex flex-row items-center gap-4 text-neutral-400">
+        <h3 className="text-xs sm:text-sm">Authored {sentQueueCount} queues</h3>
+        <h3 className="text-xs sm:text-sm ">Exported {gotCount} queues</h3>
+        <h3 className="hidden text-sm sm:block">
+          Member since {dayjs(user.createdAt).format("ll")}
+        </h3>
+      </div>
+    </header>
+  );
+};
